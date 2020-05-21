461156917511f8a97a56b03ea1bd80a26835cccb
Sharnell Grant <sharnellgrant@gmail.com>
2020-05-21 01:03:16 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -6,21 +6,47 @@ class PetsController < ApplicationController
   end
 
   get '/pets/new' do 
+    @owners = Owner.all
     erb :'/pets/new'
   end
 
   post '/pets' do 
-
+     if params[:new_owner] != ""
+      @owner = Owner.create(name: params[:new_owner])
+    else
+      @owner = Owner.find(params[:owners][:id])
+    end
+    if params[:pet][:name] != ""
+      @pet = Pet.create(name: params[:pet][:name])
+      @owner.pets << @pet
+      @owner.save
+    end
     redirect to "pets/#{@pet.id}"
   end
 
+  get '/pets/:id/edit' do
+    @pet = Pet.find(params[:id])
+    @owners = Owner.all 
+    erb :'/pets/edit'
+  end
+  
   get '/pets/:id' do 
     @pet = Pet.find(params[:id])
     erb :'/pets/show'
   end
-
+  
   patch '/pets/:id' do 
-
-    redirect to "pets/#{@pet.id}"
+   if !params[:pet].keys.include?("owner_id")
+  params[:pet]["owner_id"] = []
   end
-end
+  #######
+  @pet = Pet.find(params[:id])
+  @pet.update(params["pet"])
+  if !params["owner"]["name"].empty?
+    @pet.owner = Owner.create(name: params["owner"]["name"])
+  end
+  @pet.save
+  redirect "/pets/#{@pet.id}"
+  end
+end 
+    
